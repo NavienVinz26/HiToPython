353fd9183ddc5363b350f4082cbba429d125405c
NavienVinz26 <67771480+NavienVinz26@users.noreply.github.com>
2022-03-06 14:39:35 +0800

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,9 @@
 print('Hello,World!')
+
+def DoubleList(x):
+    Newlist = []
+    for i in range (0,len(x)):
+        result = x[i] * x[i]
+        Newlist.append(result)
+    return Newlist
+print (DoubleList([1,2,3,4]))
